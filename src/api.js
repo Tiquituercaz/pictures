d5d48fbdf446267ea9a3b9597dfd47e0a4b5aaa9
jcromerohdz <jcromerohdz@gmail.com>
2024-04-16 14:49:46 -0700

    Create ImageList, ImageShow and SearchBar components

--- src/api.js
+++ src/api.js
@@ -2,7 +2,6 @@ import axios from 'axios'
 
 const searchImage = async(term) => {
     const url = 'https://api.unsplash.com/search/photos/?client_id=6tOYXzh2nzaWSS2_6BL0Jmx-rBQDTWN2KrP2UugZYew'
-              = 'https://api.unsplash.com/search/photos/?client_id='
     const response = await axios.get(url, {
         params: {
             query: 'cars'

3f223ac67a3eb3df525f5f5b3f9428d5be26c054
jcromerohdz <jcromerohdz@gmail.com>
2024-04-18 16:45:02 -0700

    Function search picture application with React

--- src/api.js
+++ src/api.js
@@ -4,11 +4,14 @@ const searchImage = async(term) => {
     const url = 'https://api.unsplash.com/search/photos/?client_id=6tOYXzh2nzaWSS2_6BL0Jmx-rBQDTWN2KrP2UugZYew'
     const response = await axios.get(url, {
         params: {
-            query: 'cars'
+            query: term
         }
     })
 
-    console.log(response)
+    console.log(term)
+    console.log(response.data.results)
+
+    return response.data.results
 }
 
 export default searchImage

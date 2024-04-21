c056812cc72f0de577b8291826200d249e4f44a4
Tiquituercaz <pdft777@gmail.com>
2024-04-20 21:46:12 -0700

    uptade

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const searchImage = async(term) => {
-    const url = 'https://api.unsplash.com/search/photos/?client_id=6tOYXzh2nzaWSS2_6BL0Jmx-rBQDTWN2KrP2UugZYew'
+    const url = 'https://api.unsplash.com/search/photos/?client_id=c43BLTEYuulT8LO7sxZLuhGhY8PLHSC-CXXt1dQsJAA'
     const response = await axios.get(url, {
         params: {
             query: term
